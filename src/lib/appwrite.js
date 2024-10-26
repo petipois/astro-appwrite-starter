5941adaff2fd3c9789bd5f71d9e9a507ddf09b60
Petipois <petitpois24@gmail.com>
2024-10-27 01:09:23 +0200

    updates

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,7 +1,8 @@
 import { Client, Account, Databases, Storage } from "appwrite";
 const projectID = import.meta.env.PUBLIC_ASTRO_APPWRITE_PROJECT_ID;
 const endpoint = import.meta.env.PUBLIC_ASTRO_APPWRITE_ENDPOINT;
-export const client = new Client().setEndpoint(endpoint).setProject(projectID)
-export const account = new Account(client)
-export const db = new Databases(client)
-export const storage = new Storage(client)
+const client = new Client().setEndpoint(endpoint).setProject(projectID)
+const account = new Account(client)
+const db = new Databases(client)
+const storage = new Storage(client)
+export  {account, db, storage}
